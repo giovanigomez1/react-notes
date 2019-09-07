193a73ba7638ece231d837cde80571a951229dc7
Giovani Gomez <mhq2230@gmail.com>
2019-09-07 09:33:51 -0500

    inicio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
-import './App.css';
+
 
 function App() {
   return (
-  <h1>hello world</h1>
+    <Fragment>
+      <Typography align="center" variant="h2" gutterBottom>
+        Hello world
+      </Typography>
+
+    </Fragment>
   );
 }
 
