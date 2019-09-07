26e82d2940ae46569fe2a671ba8b9f5af516e1a1
Giovani Gomez <mhq2230@gmail.com>
2019-09-07 12:04:53 -0500

    Se agrega funcionalidad añadir notas a la barra izq

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
-
 import NotesForm from './NotesForm';
-
+import NoteList from './NoteList'
 
 
 class App extends Component {
-  
   constructor(props){
     super(props)
     this.state = {
@@ -29,6 +27,20 @@ updateValue = field => e =>{
 }
 
 
+saveNote = (e) =>{
+  if(this.state.title && this.state.description){
+    this.setState({
+      notes: [
+        ...this.state.notes, 
+        {
+          id: Date.now(),
+          title: this.state.title, 
+          description: this.state.description
+        }
+      ], title:"",description:""
+    })
+  }
+}
 
 
 render(){
@@ -41,20 +53,17 @@ render(){
   
         <Grid container justify='center' spacing={2}>
           <Grid item xs={4}>
-  
+            <NoteList notes={this.state.notes}/>
           </Grid>
           <Grid item xs={8}>
-            <NotesForm title={this.state.title} description={this.state.description} updateValue={this.updateValue}/>
+            <NotesForm title={this.state.title} description={this.state.description}
+             updateValue={this.updateValue} saveNote={this.saveNote}/>
           </Grid>
         </Grid>
-  
         <Fab color="primary" className="addIcon">
-  
           <AddIcon />
-  
-          
         </Fab>
-  
+
       </Fragment>
     );  
   }
